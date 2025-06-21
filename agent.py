ad1bc812ceaeda9f56b46a076a34a0ec83d4e4e8
dhirennavani <12952413+dhirennavani@users.noreply.github.com>
2025-06-21 12:49:23 -0700

    first commit

---
+++ agent.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+import os
+
+from livekit import agents
+from livekit.agents import AgentSession, Agent, RoomInputOptions
+from livekit.plugins import (
+    openai,
+    cartesia,
+    deepgram,
+    noise_cancellation,
+    silero,
+)
+from livekit.plugins.turn_detector.multilingual import MultilingualModel
+
+load_dotenv()
+
+print("Starting agent...")
+
+
+class Assistant(Agent):
+    def __init__(self) -> None:
+        super().__init__(instructions="You are a helpful voice AI assistant.")
+
+
+async def entrypoint(ctx: agents.JobContext):
+    session = AgentSession(
+        stt=openai.STT(model="gpt-4o-transcribe", language="en"),
+        llm=openai.LLM(model="gpt-4o-mini"),
+        tts=openai.TTS(model="gpt-4o-mini-tts", voice="ash",
+    instructions="Speak in a friendly and conversational tone.",),
+        vad=silero.VAD.load(),
+        turn_detection=MultilingualModel(),
+    )
+
+    await session.start(
+        room=ctx.room,
+        agent=Assistant(),
+        room_input_options=RoomInputOptions(
+            # LiveKit Cloud enhanced noise cancellation
+            # - If self-hosting, omit this parameter
+            # - For telephony applications, use `BVCTelephony` for best results
+            noise_cancellation=noise_cancellation.BVCTelephony(), 
+        ),
+    )
+
+    await ctx.connect()
+
+    await session.generate_reply(
+        instructions="Greet the user and offer your assistance."
+    )
+
+
+if __name__ == "__main__":
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
